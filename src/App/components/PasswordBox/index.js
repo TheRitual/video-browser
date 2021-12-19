32606ea953b85f9cadaed43545285cbd4a9fffaa
Marcin Kawczyński <dev@theRitual.eu>
2021-12-19 20:30:19 +0100

    Added enter reaction

--- src/App/components/PasswordBox/index.js
+++ src/App/components/PasswordBox/index.js
@@ -1,23 +1,30 @@
 import { useState } from "react";
 import { EnterButton, InputBox, InputField, PassBox } from "./styled";
 
-const PasswordBox = ({ onPasswordCheck, onHideBox, isPasswordCorrect }) => {
+const PasswordBox = ({ onPasswordCheck, onHideBox }) => {
     const [inputValue, setInputValue] = useState("");
 
     const onChangeInputValueHandler = ({ target }) => {
         setInputValue(target.value);
     }
 
+    const passwordCheckHandler = (event) => {
+        event.preventDefault();
+        onPasswordCheck(inputValue)
+    }
+
     return (
         <PassBox>
             <InputBox>
-                <InputField placeholder="Open Sesame" value={inputValue} onChange={onChangeInputValueHandler} />
-                <div>
-                    <EnterButton onClick={() => onPasswordCheck(inputValue)}> Enter </EnterButton>
-                    <EnterButton onClick={() => onHideBox()}> Close </EnterButton>
-                </div>
+                <form onSubmit={passwordCheckHandler}>
+                    <InputField placeholder="Open Sesame" value={inputValue} onChange={onChangeInputValueHandler} />
+                    <div>
+                        <EnterButton type='submit' onClick={passwordCheckHandler}> Enter </EnterButton>
+                        <EnterButton onClick={() => onHideBox()}> Close </EnterButton>
+                    </div>
+                </form>
             </InputBox>
-        </PassBox>
+        </PassBox >
     );
 }
 
